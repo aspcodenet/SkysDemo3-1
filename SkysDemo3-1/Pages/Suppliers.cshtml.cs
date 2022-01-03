0e12e9b84e2b96ab611278c686f597ef3cc5a6ec
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-01-03 11:41:44 +0100

    Sort del 1

--- SkysDemo3-1/Pages/Suppliers.cshtml.cs
+++ SkysDemo3-1/Pages/Suppliers.cshtml.cs
@@ -16,17 +16,29 @@ namespace SkysDemo3_1.Pages
         {
             public int Id { get; set; }
             public string Name { get; set; }
+            public string Country { get; set; }  
+            public string City { get; set; }
         }
 
         public List<Item> Items { get; set; }
 
-        public void OnGet()
+        public void OnGet(string sortColumn)
         {
-            Items = _context.Suppliers.Select(r => new Item
+            var query = _context.Suppliers.Select(r => new Item
             {
                 Id = r.SupplierId,
-                Name = r.CompanyName
-            }).ToList();
+                Name = r.CompanyName,
+                City = r.City,
+                Country = r.Country
+            });
+            if (sortColumn == "Country")
+                query = query.OrderBy(r => r.Country);
+            else if(sortColumn == "Namn")
+                query = query.OrderBy(r => r.Name);
+            else if (sortColumn == "City")
+                query = query.OrderBy(r => r.City);
+
+            Items = query.ToList();
         }
     }
 }
